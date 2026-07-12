21bb9c428656b034242ba8e10584d9b1aeb463bf
Sean Lam <run@slamtech.dev>
2026-07-12 19:22:52 +0800

    Rename file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from yt_downloader import download_to_mp3
 from acoustid_interface import resolve_audio_fp
 from musicbrainz_interface import resolve_release, download_cover_art
 from id3_interface import write_id3_tags
+from os.path import basename
+from os import rename
 
 
 def main(youtube_link: str):
@@ -13,6 +15,7 @@ def main(youtube_link: str):
     print(f"Audio fingerprint and metadata resolved: {metadata}")
     print("[3/4] Resolving release id...")
     album_name, release_id = resolve_release(metadata.recording_id)
+    metadata.album = album_name
     metadata.release_id = release_id
     print(f"Release id resolved: {release_id}")
     print("[4/4] Downloading cover art...")
@@ -20,7 +23,9 @@ def main(youtube_link: str):
     print(f"Cover art downloaded: {len(metadata.album_art)} bytes")
     print("[5/5] Writing ID3 tags...")
     write_id3_tags(metadata, mp3_path)
-    print(f"Done, completed mp3 file available at: {mp3_path}")
+    output_path = mp3_path.replace(basename(mp3_path), f"{metadata.title} - {metadata.artist}.mp3")
+    rename(mp3_path, output_path)
+    print(f"Done, completed mp3 file available at: {output_path}")
 
 
 if __name__ == "__main__":

e1c5b9b284da42abe0795fba22ecc774298095cf
Sean Lam <run@slamtech.dev>
2026-07-12 19:16:17 +0800

    Complete file

---
+++ id3_interface.py
@@ -0,0 +1,17 @@
+from song_metadata import SongMetadataLite
+from mutagen import ID3, TIT2, TPE1, TALB, APIC, TXXX
+
+
+def write_id3_tags(metadata: SongMetadataLite, mp3_path: str):
+    """
+    Commits the metadata to the mp3 file at the given path.
+    """
+
+    tags = ID3(mp3_path)
+    tags.add(TIT2(encoding=3, text=metadata.title))
+    tags.add(TPE1(encoding=3, text=metadata.artist))
+    tags.add(TALB(encoding=3, text=metadata.album))
+    tags.add(APIC(encoding=3, mime="image/jpeg", type=3, desc="Cover", data=metadata.album_art))
+    tags.add(TXXX(encoding=3, desc="Recording ID", text=metadata.recording_id))
+    tags.add(TXXX(encoding=3, desc="Release ID", text=metadata.release_id))
+    tags.save(mp3_path)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from yt_downloader import download_to_mp3
 from acoustid_interface import resolve_audio_fp
-from musicbrainz_interface import resolve_release_id, download_cover_art
+from musicbrainz_interface import resolve_release, download_cover_art
+from id3_interface import write_id3_tags
 
 
 def main(youtube_link: str):
@@ -11,12 +12,15 @@ def main(youtube_link: str):
     metadata = resolve_audio_fp(mp3_path)
     print(f"Audio fingerprint and metadata resolved: {metadata}")
     print("[3/4] Resolving release id...")
-    release_id = resolve_release_id(metadata.recording_id)
+    album_name, release_id = resolve_release(metadata.recording_id)
     metadata.release_id = release_id
     print(f"Release id resolved: {release_id}")
     print("[4/4] Downloading cover art...")
     metadata.album_art = download_cover_art(release_id)
     print(f"Cover art downloaded: {len(metadata.album_art)} bytes")
+    print("[5/5] Writing ID3 tags...")
+    write_id3_tags(metadata, mp3_path)
+    print(f"Done, completed mp3 file available at: {mp3_path}")
 
 
 if __name__ == "__main__":

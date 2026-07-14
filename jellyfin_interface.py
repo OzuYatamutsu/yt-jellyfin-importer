17bcd6e966749292cdd8629197355a645c6a4626
Sean Lam <run@slamtech.dev>
2026-07-14 12:59:16 +0800

    Scaffolding for moving to jellyfin

---
+++ jellyfin_interface.py
@@ -0,0 +1,5 @@
+from song_metadata import SongMetadataLite
+
+
+def move_file_to_jellyfin_dir(metadata: SongMetadataLite) -> void:
+    pass  # TODO
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from yt_downloader import download_to_mp3
-from acoustid_interface import resolve_audio_fp
 from musicbrainz_interface import resolve_release, download_cover_art
+from jellyfin_interface import move_file_to_jellyfin_dir
+from acoustid_interface import resolve_audio_fp
+from yt_downloader import download_to_mp3
+
 from id3_interface import write_id3_tags
 from argparse import ArgumentParser
 from typing import Optional
@@ -13,24 +15,33 @@ def main(youtube_link: str, jellyfin_lib_location: Optional[str]):
     print("[1/5] Downloading and converting to mp3...")
     mp3_path = download_to_mp3(youtube_link)
     print(f"Downloaded to {mp3_path}")
+
     print("[2/5] Generating audio fingerprint and resolving initial metadata...")
     metadata = resolve_audio_fp(mp3_path)
     print(f"Audio fingerprint and metadata resolved: {metadata}")
     if not metadata:
         print("No metadata found!! Aborting!!")
         exit(1)
+
     print("[3/5] Resolving release...")
     metadata = resolve_release(metadata.recording_id, metadata)
     print(f"Release information resolved.")
+
     print("[4/5] Downloading cover art...")
     metadata.album_art = download_cover_art(metadata.release_id)
     print(f"Cover art downloaded: {len(metadata.album_art)} bytes")
+
     print("[5/5] Writing ID3 tags...")
     write_id3_tags(metadata, mp3_path)
     output_path = mp3_path.replace(basename(mp3_path), f"{metadata.artist} - {metadata.title}.mp3")
     output_path = output_path.replace(config.DOWNLOAD_LOCATION, config.OUTPUT_LOCATION)
     rename(mp3_path, output_path)
     print(f"Done, completed mp3 file available at: {output_path}")
+    
+    if jellyfin_lib_location:
+        print("Moving to jellyfin...")
+        output_path = move_file_to_jellyfin_dir(metadata)
+        print(f"Done, moved mp3 file to {output_path}.")
 
 
 def _set_config_from_args() -> None:

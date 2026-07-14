e3ebed762b468f36b9d9dd1d555e11aba7544f62
Sean Lam <run@slamtech.dev>
2026-07-14 12:26:25 +0800

    Read config from env

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import environ
 
-DOWNLOAD_LOCATION = environ["TMPDIR"]
+DOWNLOAD_LOCATION = environ.get("DOWNLOAD_LOCATION", environ["TMPDIR"])
 
 # Get one from here: https://acoustid.org/new-application
-ACOUSTID_API_KEY = "insert_api_key_here"
+ACOUSTID_API_KEY = environ.get("ACOUSID_API_KEY", "insert_api_key_here") 

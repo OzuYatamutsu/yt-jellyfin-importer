f28d1aed15629fb82824ff778794a13ce2b56ca7
Sean Lam <run@slamtech.dev>
2026-07-18 10:51:39 +0800

    Remove release year

--- downloaderbe/jellyfin_interface.py
+++ downloaderbe/jellyfin_interface.py
@@ -9,7 +9,7 @@ def move_file_to_jellyfin_dir(metadata: SongMetadataLite) -> str:
     target_path = Path(
         config.JELLYFIN_LIBRARY
         + f"/{metadata.album_artist}"
-        + f"/{metadata.album} ({metadata.release_year})"
+        + f"/{metadata.album}"
     )
     target_path.mkdir(
         parents=True, exist_ok=True
